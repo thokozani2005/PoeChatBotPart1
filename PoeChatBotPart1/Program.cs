5efc57ad452547a2d0ba77db2d25205452e19fea
mlamb <mlamb@BRIANSHP>
2025-03-30 23:35:43 +0200

    Add project files.

---
+++ PoeChatBotPart1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoeChatBotPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //called my classes in my mainMethod
+            new Voice() { };
+        }
+    }
+}
---
+++ PoeChatBotPart1/Voice.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Media;
+using System;
+
+namespace PoeChatBotPart1
+{
+    public class Voice
+    {
+        //built in constructor
+        public Voice()
+        {
+            //getting the full location
+            string full_location = AppDomain.CurrentDomain.BaseDirectory;
+
+            Console.WriteLine(full_location);
+            //using a new filePath
+            string new_path = full_location.Replace("bin\\Debug", "");
+
+            //used exception handling
+            try
+            {
+                //Playing the voice over if the file is found
+                string full_path = Path.Combine(new_path, "Welcome.wav");
+                using (SoundPlayer myplayer = new SoundPlayer(full_path))
+                {
+                    myplayer.PlaySync();
+                }
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+
+        }
+    }
+}
