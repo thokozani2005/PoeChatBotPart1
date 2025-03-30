dac36a4f2196c81ecfef5f04e3dd17d85be2bf77
Thokozani Mlambo <mlambobrian233@gmail.com>
2025-03-31 00:01:22 +0200

    created another external class that will store methods that validate user inputs and methods that enhance the bot

--- PoeChatBotPart1/Program.cs
+++ PoeChatBotPart1/Program.cs
@@ -15,8 +15,17 @@ namespace PoeChatBotPart1
             new Voice() { };
             //Logo class
             new Logo_Image() { };
+            
+
             //called this class that does not have a constructor
-          
+            Store_ValidationMethods validate = new Store_ValidationMethods();
+
+            //used the typing effect method to enhance the Bot
+            validate.AddBotTypingEffect("************************************************************************\nWelcome to the Cyber Security Awareness Bot (Cyber Sheild) \n************************************************************************", ConsoleColor.Green);
+
+            //used the typing effect method to enhance the Bot
+            validate.AddBotTypingEffect("Bot: Hello! I'm here to help you stay safe online.", ConsoleColor.Yellow);
+
 
 
 
---
+++ PoeChatBotPart1/Store_ValidationMethods.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoeChatBotPart1
+{
+    //created an external class where i am going to store the methods that validate user input and the Typing effect method
+    class Store_ValidationMethods
+    {
+
+        //created a boolean method whill check if the  name is empty or has double spaces
+        public Boolean CheckUserName(string user_Name)
+        {
+            //checking if the user's name is empty
+            if (string.IsNullOrEmpty(user_Name))
+            {
+                TriggerBeep();
+                AddBotTypingEffect("Bot: Please enter a valid username, username cannot be empty", ConsoleColor.DarkRed);
+
+
+                return false;
+            }
+            // Checking if the user's name is double spaces 
+            else if (user_Name.Contains("  "))
+            {
+
+                AddBotTypingEffect("Bot: Please enter a valid username, username cannot contain double spaces", ConsoleColor.DarkRed);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        //creating a method that will check if the question is not or not
+
+        public Boolean VerifyUserQuestion(String Question)
+        {
+            if (string.IsNullOrEmpty(Question))
+            {
+                TriggerBeep();
+                AddBotTypingEffect("Bot: Please enter a valid Question, Question cannot be empty", ConsoleColor.DarkRed);
+                return false;
+            }
+            else if (Question.Contains("  "))
+            {
+                AddBotTypingEffect("Bot: Please enter a valid Question, Question cannot contain double spaces", ConsoleColor.DarkRed);
+                return false;
+            }
+
+            return true;
+        }
+
+
+        //created a method that changes the Bot typing effect, making it more user friendly
+        //passed the message,message color and the speed of the message as parameters
+        public void AddBotTypingEffect(string Bot_message, ConsoleColor text_color, int text_speed = 30)// this method parses a string
+        {
+            Console.ForegroundColor = text_color;
+            foreach (char messages in Bot_message)
+            {
+                Console.Write(messages);
+                System.Threading.Thread.Sleep(text_speed); // Adjust speed for effect
+            }
+            Console.WriteLine();
+            Console.ResetColor();
+        }
+
+        /*
+         *created a method will trigger a sound when user input is wrong
+        passed two parameters the first one is for the frequency, it controls the pitch of the sound
+        second parameter is the duration, it is for the time the sound will take
+        
+         */
+        public void TriggerBeep(int frequency = 500, int duration = 1000)
+        {
+            //used exception Handling
+            try
+            {
+                // This will play a beep with the specified frequency and duration
+                Console.Beep(frequency, duration);
+                //Console.ForegroundColor = ConsoleColor.Yellow;
+                //Console.WriteLine("Beep sound triggered!");
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"An error occurred while playing the beep: {ex.Message}");
+            }
+        }
+
+
+    }
+}
